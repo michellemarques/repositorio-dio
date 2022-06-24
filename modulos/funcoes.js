b301d811062493f952a9c277827e1a5143a313c9
Michelle Marques <michellea.m.silva@gmail.com>
2022-06-24 16:28:37 -0300

    Exercícios de módulos

---
+++ modulos/funcoes.js
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}.` 
+};
+
+function mostraICidade(nome, idade) {
+    return `A cidade de ${nome} é ${idade}.` 
+};
+
+function mostraHobby(nome, idade) {
+    return `O hobby de ${nome} é ${idade}.` 
+};
+
+export {
+    mostraIdade,
+    mostraICidade,
+    mostraHobby
+};
---
+++ modulos/main.js
@@ -0,0 +1,5 @@
+import{ mostraIdade, mostraICidade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade('Marco', 15));
+console.log(mostraICidade('Aline', 'Alagoas'));
+console.log(mostraHobby('Ana', 'Game'));
