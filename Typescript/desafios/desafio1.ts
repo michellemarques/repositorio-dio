51037b3308bd27e9d051d01236804cf622bbf0bf
Michelle Marques <michellea.m.silva@gmail.com>
2022-07-01 16:39:14 -0300

    Desafios

---
+++ Typescript/desafios/desafio1.ts
@@ -0,0 +1,22 @@
+/* // Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John"; */
+
+let employee: { code: number, name: string } = {
+    code: 10,
+    name: 'Jhon'
+}
+employee.code = 10;
+employee.name = "John"; 
+
+//   OU
+
+interface employee {
+    code: number,
+    name: string
+};
+
+employee.code = 10;
+employee.name = 'Alberto';
---
+++ Typescript/desafios/desafio2.ts
@@ -0,0 +1,58 @@
+/* // Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+} */
+
+interface IPessoas {
+    nome: string,
+    idade: number,
+    profissao: Profissoes
+};
+
+enum Profissoes {
+    Atriz,
+    Padeiro
+}
+
+let pessoa1: IPessoas = {
+    nome: 'maria',
+    idade: 29,
+    profissao: Profissoes.Atriz
+};
+
+let pessoa2: IPessoas = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: Profissoes.Padeiro
+};
+
+let pessoa3: IPessoas = {
+    nome: 'laura',
+    idade: 31,
+    profissao: Profissoes.Atriz
+};
+
+let pessoa4: IPessoas = {
+    nome: 'carlos',
+    idade: 19,
+    profissao: Profissoes.Padeiro
+};
