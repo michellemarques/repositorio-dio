931e8b058a9d94113c69070154b1b31523234fb7
Michelle Marques <michellea.m.silva@gmail.com>
2022-07-07 17:52:46 -0300

    Criação de nav bar e rotas

---
+++ angular-8/course-manager-1/src/app/error-404/error-404.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({ 
+    /* ñ é necessário selector*/ 
+    templateUrl: './error-404.component.html'
+})
+export class Error404Component {
+
+}
