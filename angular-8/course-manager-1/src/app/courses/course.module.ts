4b5e0f658c205c6eb18a977da7f9d64603708a62
Michelle Marques <michellea.m.silva@gmail.com>
2022-07-11 17:30:37 -0300

    Criando a pasta shared (onde ficam os módulos mais genérico da aplicação)

--- angular-8/course-manager-1/src/app/courses/course.module.ts
+++ angular-8/course-manager-1/src/app/courses/course.module.ts
@@ -2,21 +2,21 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../start/star.component";
+import { StarModule } from "../shered/component/start/star.module";
+import { AppPipeModule } from "../shered/pipe/app.pipe.module";
 import { CourseListComponent } from "./course-list.component";
 import { CourseInfoComponent } from "./course.info.component";
 
 @NgModule({
     declarations: [
         CourseListComponent,
-        CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
+        CourseInfoComponent
 
     ], imports: [
         FormsModule,
         CommonModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
