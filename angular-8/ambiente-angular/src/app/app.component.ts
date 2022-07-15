78d1bb59854bd44f6eb7286e3d5336724df5c054
Michelle Marques <michellea.m.silva@gmail.com>
2022-07-15 19:18:12 -0300

    Directives e manipulação de atributos

--- angular-8/ambiente-angular/src/app/app.component.ts
+++ angular-8/ambiente-angular/src/app/app.component.ts
@@ -8,13 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   count = 0;
 
+  nome = 'Ana Maria da Silva Costa';
+
+  text = '';
+
+  pessoas = [
+    {
+      nome: 'Ivo',
+      sobrenome: 'Soares'
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silva'
+    },
+    {
+      nome: 'João',
+      sobrenome: 'Pereira'
+    },
+    {
+      nome: 'Márcio',
+      sobrenome: 'Santos'
+    }
+];
+
   constructor() {}
 
   ngOnInit(): void {
+      console.log(this.pessoas);
+
       let interval = setInterval(() => {
         this.count++
         if(this.count === 10) clearInterval(interval);
       }, 1000);
+  };
+
+  clicou(nome: string): void {
+    console.log("Clicou no botão", nome);
   }
 
 }
